ebe6b7ae62f1b198d0091ba538640636f566f334
luanon404 <luanon404@gmail.com>
2023-11-30 21:31:17 +0700

    Add navigation

--- scripts/webidl/convert.js
+++ scripts/webidl/convert.js
@@ -155,6 +155,7 @@ addDir("../../lib/jsdom/living/file-api");
 addDir("../../lib/jsdom/living/geometry");
 addDir("../../lib/jsdom/living/hr-time");
 addDir("../../lib/jsdom/living/mutation-observer");
+addDir("../../lib/jsdom/living/navigation");
 addDir("../../lib/jsdom/living/navigator");
 addDir("../../lib/jsdom/living/nodes");
 addDir("../../lib/jsdom/living/range");
